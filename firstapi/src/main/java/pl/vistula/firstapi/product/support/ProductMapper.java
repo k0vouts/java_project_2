16d3f484ed571c0de35abb1caf6c2739d4c845d2
k0vouts <konvoutskoglou@gmail.com>
2026-01-11 16:50:14 +0100

    commit c

--- firstapi/src/main/java/pl/vistula/firstapi/product/support/ProductMapper.java
+++ firstapi/src/main/java/pl/vistula/firstapi/product/support/ProductMapper.java
@@ -9,19 +9,16 @@ import pl.vistula.firstapi.product.domain.Product;
 @Component
 public class ProductMapper {
 
-    // Method 1: Convert ProductRequest to Product (for POST)
     public Product toProduct(ProductRequest productRequest) {
         Product product = new Product();
         product.setName(productRequest.getName());
         return product;
     }
 
-    // Method 2: Convert Product to ProductResponse (for GET/POST/PUT)
     public ProductResponse toProductResponse(Product product) {
         return new ProductResponse(product.getId(), product.getName());
     }
 
-    // Method 3: Update Product and return ProductResponse (for PUT)
     public ProductResponse toProductResponse(Product product, UpdateProductRequest request) {
         product.setName(request.getName());
         return new ProductResponse(product.getId(), product.getName());
